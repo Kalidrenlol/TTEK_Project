3addc2811652907e90a07e3d211e60308bc6058b
Nicklas Frederiksen <nfrederiksen@hotmail.com>
2016-10-06 17:47:58 +0200

    Network Part Added

---
+++ Augmented Reality Game/Assets/Scripts/HostGame.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class HostGame : MonoBehaviour {
+
+	[SerializeField] private uint roomSize = 6;
+
+	private string roomName;
+	private NetworkManager networkManager;
+
+	void Start() {
+		networkManager = NetworkManager.singleton;
+		if (networkManager.matchMaker == null) {
+			networkManager.StartMatchMaker();
+		}
+	}
+
+	public void SetRoomName(string _name) {
+		Debug.Log("Kaldt");
+		roomName = _name;
+	}
+
+	public void CreateRoom() {
+
+		Debug.Log("Navn: "+roomName);
+		if (roomName != null && roomName != "") {
+			Debug.Log("Creating a room: "+roomName + " with room for " + roomSize + " players.");
+
+			networkManager.matchMaker.CreateMatch(roomName, roomSize, true, "", "", "", 0, 0, networkManager.OnMatchCreate);
+		}
+	}
+}
