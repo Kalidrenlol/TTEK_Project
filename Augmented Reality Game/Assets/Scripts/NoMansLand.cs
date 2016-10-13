330ace268c62fa57a8095eae985764201aad737f
Emil Lindgren Ipsen <ei@jbg54.local>
2016-10-13 18:11:49 +0200

    Started implementing OutOfBounds-area

---
+++ Augmented Reality Game/Assets/Scripts/NoMansLand.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoMansLand : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision collision) {
+
+
+        if (collision.collider.tag == "Player")
+        {
+            Debug.Log("Player \""+collision.collider.name+"\" hit outside");
+        }
+
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+
+
+        if (collision.collider.tag == "Player")
+        {
+            Debug.Log("Player \"" + collision.collider.name + "\" left outside");
+        }
+
+    }
+
+}
