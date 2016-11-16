f30c54dc3b356c8d21bae1e9adceb5cf7f7c0733
EmilIpsen <emillipsen@gmail.com>
2016-11-16 12:51:21 +0100

    Started implementing camera-direction movement

--- Augmented Reality Game/Assets/Scripts/Player/PlayerController.cs
+++ Augmented Reality Game/Assets/Scripts/Player/PlayerController.cs
@@ -24,7 +24,28 @@ public class PlayerController : MonoBehaviour {
 		float _zMov = joystick.GetComponent<VirtualJoystick>().Vertical();
 
 		Vector3 _velocity = new Vector3 (_xMov, 0, _zMov);
+        var cam = Camera.main.transform;
+        Vector3 camVec = cam.forward;
+        //Debug.Log("v: " + _velocity);
+        //Debug.Log("c: " + camVec); // Bevæg i forhold til X og Z (X, Y, Z)
+        Vector3 camBase = new Vector3(cam.position.x, 0.2f, cam.position.z);
+        Vector3 camDir  = new Vector3(camVec.x*100,0.2f,camVec.z*100);
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            cam.Rotate(Vector3.down, 100 * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            cam.Rotate(Vector3.up, 100 * Time.deltaTime);
+        }
+        float camAngle  = Vector3.Angle(camBase, camDir);
+        Debug.Log("Angle: " + camAngle);
 
+        // Rotatet _velocity to align with camAngle
+
+
+        Debug.DrawRay(camBase, camDir, Color.green,5,false);
+        _velocity = Quaternion.Euler(0, camAngle+180, 0) * _velocity;
 		motor.Move(_velocity);
 
 
