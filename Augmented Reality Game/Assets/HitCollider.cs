2e09c9b287ff8746e4a892ae5a664860f4ee9728
Nicklas Frederiksen <nfrederiksen@hotmail.com>
2016-12-15 10:24:43 +0100

    HitCollider - Test

---
+++ Augmented Reality Game/Assets/HitCollider.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitCollider : MonoBehaviour {
+
+	public bool isInRange;
+
+	// Use this for initialization
+	void Start () {
+		isInRange = false;
+	}
+
+	void OnTriggerEnter(Collider coll) {
+		if (coll.gameObject.tag == "Player") {
+			Debug.Log(coll.name +" er inden for rækkevidde");
+			isInRange = true;
+		}
+
+		Debug.Log(this);
+		Debug.Log(coll.gameObject);
+
+		if (coll.gameObject == this) {
+			Debug.Log("Sig selv");
+		}
+	}
+
+	void OnTriggerExit(Collider coll) {
+		if (coll.gameObject.tag == "Player") {
+			Debug.Log(coll.name + " er uden for rækkevidde");
+			isInRange = false;
+		}
+	}
+
+}
