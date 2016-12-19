e498ff9d651027cdb5b72aebf0a80bb01485f00a
EmilIpsen <emillipsen@gmail.com>
2016-12-19 16:47:50 +0100

    Screenshake, PU shiffle almost working

---
+++ Augmented Reality Game/Assets/DestroyThis.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyThis : MonoBehaviour {
+
+    public float DestroyInSeconds = 3f;
+ 
+	// Use this for initialization
+	void Awake () {
+        Invoke("Destroy", DestroyInSeconds);
+	}
+
+    void Destroy()
+    {
+        Destroy();
+    }
+}
